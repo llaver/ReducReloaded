20f7e26aa73e7cf76c969f2ef150d6691242ee7b
Llaver <Llaver@live.com>
2017-07-06 01:01:18 -0500

    Created PlayGame and Settings Activities. Main Menu buttons now start those activities.

--- app/src/main/java/com/game/alpha/reduc/MainMenu.java
+++ app/src/main/java/com/game/alpha/reduc/MainMenu.java
@@ -1,13 +1,37 @@
 package com.game.alpha.reduc;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainMenu extends AppCompatActivity {
+public class MainMenu extends Activity {
+	
+	//TODO Get rid of all the bs AppCompat shit I accidentally left on by default
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main_menu);
+		
+		//INTENTS I GUESS
+		final Intent playGameIntent = new Intent(this, PlayGameActivity.class); //TODO well it would probably be a good idea to come up with a game......
+		final Intent settingsIntent = new Intent(this, SettingsActivity.class); //TODO LET THE PEOPLE CHOOSE
+		
+		final Button button = (Button) findViewById(R.id.button);
+		button.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				startService(playGameIntent);
+			}
+		});
+		final Button button2 = (Button) findViewById(R.id.button2);
+		button.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				startActivity(settingsIntent);
+			}
+		});
+		
 	}
 }
---
+++ app/src/main/java/com/game/alpha/reduc/PlayGameActivity.java
@@ -0,0 +1,13 @@
+package com.game.alpha.reduc;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PlayGameActivity extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_play_game);
+	}
+}
