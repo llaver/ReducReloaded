6d288cd8df507a7305775e2e8be70fdf3c1a42e0
Llaver <Llaver@live.com>
2017-07-27 21:24:39 -0500

    WE FIXED IT SOMEHOW BOYS

---
+++ app/src/main/java/com/game/alpha/reduc/game/GlobalVars.java
@@ -0,0 +1,7 @@
+package com.game.alpha.reduc.game;
+
+/**
+ * Created by rbell on 7/26/2017.
+ */
+public class GlobalVars {
+}
---
+++ app/src/main/java/com/game/alpha/reduc/game/entities/Road.java
@@ -0,0 +1,7 @@
+package com.game.alpha.reduc.game.entities;
+
+/**
+ * Created by rbell on 7/26/2017.
+ */
+public class Road {
+}
