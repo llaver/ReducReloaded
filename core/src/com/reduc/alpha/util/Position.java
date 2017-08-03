5f9627d6f12664181e7ba4cf9f18bc26f3363e74
Llaver <Llaver@live.com>
2017-08-03 18:04:40 -0500

    Position and RoadPos finished and now fully documented.

--- core/src/com/reduc/alpha/util/Position.java
+++ core/src/com/reduc/alpha/util/Position.java
@@ -1,6 +1,8 @@
 package com.reduc.alpha.util;
 
 /**
+ * Position class
+ *
  * Created by rbell on 7/14/2017.
  */
 public class Position {
@@ -8,11 +10,20 @@ public class Position {
 	private float x;
 	private float y;
 	
+	/**
+	 * Create a default Position
+	 */
 	public Position() {
 		x = 0;
 		y = 0;
 	}
 	
+	/**
+	 * Create a Position with the specified coordinates
+	 *
+	 * @param x The X coordinate
+	 * @param y The Y coordinate
+	 */
 	public Position(float x, float y) {
 		this.x = x;
 		this.y = y;
@@ -34,16 +45,18 @@ public class Position {
 		this.y = y;
 	}
 	
-	@Override
+	/**
+	 * Compares the coordinates of two position objects
+	 *
+	 * @param obj the object to be compared to
+	 * @return True if the coordinates are the same
+	 */
 	public boolean equals(Object obj) {
-		if(obj instanceof Position) {
-			return(x == ((Position) obj).getX() && y == ((Position) obj).getY());
-		}
-		return false;
+		return obj instanceof Position && (x == ((Position) obj).getX() && y == ((Position) obj).getY());
 	}
 	
 	@Override
 	public String toString() {
-		return super.toString();
+		return "X: " + x + " Y: " + y;
 	}
 }
--- core/src/com/reduc/alpha/util/RoadPos.java
+++ core/src/com/reduc/alpha/util/RoadPos.java
@@ -1,7 +1,9 @@
 package com.reduc.alpha.util;
 
 /**
- * Created by rbell on 7/14/2017.
+ * Road Position Util Class
+ * <p>
+ * Created by Ryan Bell
  */
 public class RoadPos {
 	
@@ -10,6 +12,9 @@ public class RoadPos {
 	private Position topLeft;
 	private Position topRight;
 	
+	/**
+	 * Create a new default RoadPos
+	 */
 	public RoadPos() {
 		bottomLeft = new Position();
 		bottomRight = new Position();
@@ -17,6 +22,18 @@ public class RoadPos {
 		topRight = new Position();
 	}
 	
+	/**
+	 * Create a new RoadPos with individual x and y coordinates for all 4 corners
+	 *
+	 * @param blX Bottom Left X
+	 * @param blY Bottom Left Y
+	 * @param brX Bottom Right X
+	 * @param brY Bottom Right Y
+	 * @param tlX Top Left X
+	 * @param tlY Top Left Y
+	 * @param trX Top Right X
+	 * @param trY Top Right Y
+	 */
 	public RoadPos(float blX, float blY, float brX, float brY, float tlX, float tlY, float trX, float trY) {
 		bottomLeft = new Position(blX, blY);
 		bottomRight = new Position(brX, brY);
@@ -24,6 +41,14 @@ public class RoadPos {
 		topRight = new Position(tlX, tlY);
 	}
 	
+	/**
+	 * Create a new RoadPos with 4 corner Position objects
+	 *
+	 * @param bl Bottom Left
+	 * @param br Bottom Right
+	 * @param tl Top Left
+	 * @param tr Top Right
+	 */
 	public RoadPos(Position bl, Position br, Position tl, Position tr) {
 		bottomLeft = bl;
 		bottomRight = br;
@@ -31,32 +56,64 @@ public class RoadPos {
 		topRight = tr;
 	}
 	
-	public Position[] calculatePosition(RoadPos rp, Position endPoint) {
+	/**
+	 * Create a new RoadPos with an array of Position objects
+	 * <p>
+	 * Order should always be Bottom Left, Bottom Right, Top Left, Top Right
+	 * This should mainly be used with the calculatePosition() method.
+	 *
+	 * @param position An array of position objects. Best obtained from calculatePosition().
+	 */
+	public RoadPos(Position[] position) {
+		bottomLeft = position[0];
+		bottomRight = position[1];
+		topLeft = position[2];
+		topRight = position[3];
+	}
+	
+	/**
+	 * Returns an array of Position objects of the next Road section
+	 *
+	 * @param previous The RoadPos object of the road section directly prior to the one being calculated
+	 * @param endPoint The center point along the line that is the end of the road.
+	 * @return An array of the four corner positions of the next road section
+	 */
+	public static Position[] calculatePosition(RoadPos previous, Position endPoint) {
 		Position[] newPos = new Position[4];
 		
 		float xDiff = newPos[1].getX() - endPoint.getX();
 		double rad = Math.toRadians(2.5);
 		
-		newPos[0] = rp.getTopLeft();
-		newPos[1] = rp.getTopRight();
+		newPos[0] = previous.getTopLeft();
+		newPos[1] = previous.getTopRight();
 		newPos[2] = new Position((float) (xDiff * Math.cos(rad)), endPoint.getY());
 		newPos[3] = new Position((float) (xDiff * Math.cos(-rad)), endPoint.getY());
 		
 		return newPos;
 	}
 	
-	@Override
+	/**
+	 * Compares two RoadPos objects
+	 *
+	 * @param obj The object to be compared
+	 * @return True if all four corner positions match up.
+	 */
 	public boolean equals(Object obj) {
-		if(obj instanceof RoadPos) {
-			return(bottomLeft.equals(((RoadPos) obj).getBottomLeft()) && bottomRight.equals(((RoadPos) obj).getBottomRight())
-			&& topLeft.equals(((RoadPos) obj).getTopLeft()) && topRight.equals(((RoadPos) obj).getTopRight()));
-		}
-		return false;
+		return obj instanceof RoadPos
+			&& (bottomLeft.equals(((RoadPos) obj).getBottomLeft())
+			&& bottomRight.equals(((RoadPos) obj).getBottomRight())
+			&& topLeft.equals(((RoadPos) obj).getTopLeft())
+			&& topRight.equals(((RoadPos) obj).getTopRight()));
 	}
 	
-	@Override
+	/**
+	 * Lets 3D print some roads yo
+	 *
+	 * @return A string containing the coordinates of all four corner positions
+	 */
 	public String toString() {
-		return super.toString();
+		return "Bottom Left " + bottomLeft.toString() + " Bottom Right " + bottomRight.toString()
+			+ " Top Left " + topLeft.toString() + " Top Right " + topRight.toString() + ".";
 	}
 	
 	public Position getBottomLeft() {
