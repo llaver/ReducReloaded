fdd7c7ce2b87c046251022a4dd9011d16bdb5830
Llaver <Llaver@live.com>
2017-08-09 00:55:09 -0500

    Introduced OpenSimplexNoise. Next step is getting A* pathing to generate my road :)

--- core/src/com/reduc/alpha/Test.java
+++ core/src/com/reduc/alpha/Test.java
@@ -1,10 +1,28 @@
 package com.reduc.alpha;
 
+import com.reduc.alpha.util.OpenSimplexNoise;
+
+
 /**
  * Created by rbell on 8/3/2017.
  */
 public class Test {
 	public static void main(String[] args) {
-	
+		OpenSimplexNoise noise = new OpenSimplexNoise();
+		Double[][] map = new Double[56][56];
+		
+		for(int i = 0; i < map.length; i++) {
+			System.out.print("\n*");
+			for(int k = 0; k < map[i].length; k++) {
+				map[i][k] = noise.eval(i, k);
+				double current = map[i][k];
+				//System.out.println(current);
+				if(current <= .2 && current >= -.2) {
+					System.out.print("X");
+				} else {
+					System.out.print("O");
+				}
+			}
+		}
 	}
 }
